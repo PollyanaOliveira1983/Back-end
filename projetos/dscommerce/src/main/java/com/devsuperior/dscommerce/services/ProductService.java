feb31d97ccac02b85918c3762f9c63c247e07126
Pollyana <pollyana.oliveira@gmail.com>
2022-12-05 15:02:13 -0300

    DELETE  product

--- projetos/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ projetos/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -46,10 +46,17 @@ public class ProductService {
         return new ProductDTO(entity);
     }
 
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
     private void copyDtoEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
     }
+
+
 }
