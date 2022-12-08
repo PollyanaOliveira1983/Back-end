cc2658d5ac1abf559b2ec6994139f31db76e358e
Pollyana <pollyana.oliveira@gmail.com>
2022-12-08 11:06:58 -0300

    aula salvar para um

---
+++ projetos/aula-salvar-para-um/src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -0,0 +1,38 @@
+package com.devsuperior.aula.services;
+
+import com.devsuperior.aula.dto.PersonDepartmentDTO;
+import com.devsuperior.aula.entities.Department;
+import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.DepartmentRepository;
+import com.devsuperior.aula.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private PersonRepository repository;
+
+ //entidade pessoa associada com o departamento para salvar no bd
+    public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
+        Person entity = new Person();
+        entity.setName(dto.getName());
+        entity.setSalary(dto.getSalary());
+
+        Department dept = departmentRepository.getReferenceById(dto.getDepartment().getId());
+
+       // Department dept = new Department();
+       // dept.setId(dto.getDepartment().getId());
+
+        entity.setDepartment(dept);
+
+        entity = repository.save(entity);
+
+        return new PersonDepartmentDTO(entity);
+
+    }
+}
