ab092742654273094d5cc0cddee007f857964e82
Pollyana <pollyana.oliveira@gmail.com>
2022-12-15 14:38:56 -0300

    Checklist do OAuth2 com JWT no Spring Boot

--- projetos/dscommerce/src/main/java/com/devsuperior/dscommerce/config/WebSecurityConfig.java
+++ projetos/dscommerce/src/main/java/com/devsuperior/dscommerce/config/WebSecurityConfig.java
@@ -1,27 +1,41 @@
 package com.devsuperior.dscommerce.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
 	@Bean
-	BCryptPasswordEncoder bCryptPasswordEncoder() {
+	BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		//permite acesso a toda requisição
-	http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-		//libera o acesso ao banco h2
-	http.headers().frameOptions().disable();
-		//libera a protecao csrf protege do racker
-	http.csrf().disable();
-	return http.build();
+	JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(jwtSecret);
+		return tokenConverter;
+	}
+
+	@Bean
+	JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
