00a47632fff18605a3af7c612264c31e787827f8
Pollyana <pollyana.oliveira@gmail.com>
2022-12-15 17:46:49 -0300

    Getting user logged

--- projetos/dscommerce/src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ projetos/dscommerce/src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscommerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscommerce.dto.UserDTO;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.repositories.UserRepository;
 
@@ -23,5 +26,21 @@ public class UserService implements UserDetailsService {
 			throw new UsernameNotFoundException("Email not found");
 		}
 		return user;
+	}
+	
+	protected User authenticated() {
+		try {
+			String username = SecurityContextHolder.getContext().getAuthentication().getName();
+			return repository.findByEmail(username);
+		}
+		catch (Exception e) {
+			throw new UsernameNotFoundException("Invalid user");
+		}
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO getMe() {
+		User entity = authenticated();
+		return new UserDTO(entity);
 	}
 }
