4edd6c4a2817569c5b22c3348ce2694638e74332
Pollyana <pollyana.oliveira@gmail.com>
2022-12-19 17:13:48 -0300

    Validate self or admin

--- projetos/dscommerce/src/main/java/com/devsuperior/dscommerce/services/OrderService.java
+++ projetos/dscommerce/src/main/java/com/devsuperior/dscommerce/services/OrderService.java
@@ -33,10 +33,14 @@ public class OrderService {
 	 @Autowired
 	 private UserService userService;
 	 
+	 @Autowired
+	 private AuthService authService;
+	 
 	 @Transactional(readOnly = true)
 	 public OrderDTO findById(Long id) {
 	      Order order = repository.findById(id).orElseThrow(
 	              () -> new ResourceNotFoundException("Recurso não encontrado"));
+	      authService.validateSelfOeAdmin(order.getClient().getId());
 	 return new OrderDTO(order);
 	 }
 
